08b9648e6122c710d5f480fc3425784c4fa4035e
Jace Ju <jaceju@gmail.com>
2015-07-25 23:16:03 +0800

    Remove local server on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+process.env.DISABLE_NOTIFIER = true;
+
 var elixir = require('laravel-elixir');
 require('laravel-elixir-wiredep');
 require('laravel-elixir-browser-sync');
@@ -19,10 +21,5 @@ elixir(function (mix) {
     if (elixir.config.production) {
         mix.useref({ src: false })
             .version(['js/*.js', 'css/*.css'])
-    } else {
-        mix.browserSync(null, {
-            proxy: 'chat-room.app',
-            reloadDelay: 2000
-        });
     }
 });
